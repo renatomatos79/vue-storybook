756fef695ecf7727708627599ced999d1c023d13
renato matos <renato.matos@specsavers.com>
2021-10-14 23:33:35 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <MyButton :isLoading=true>
+      <img src="../assets/logo.png" alt="logo">
+  </MyButton>
+</template>
+
+<script>
+import MyButton from './MyButton.vue';
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  components: {
+    MyButton
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
